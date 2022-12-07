52aaea2bb2c1a7b3f2bb28908c989434572adb57
Duy Phan <duy.910.2015@gmail.com>
2022-12-07 08:55:53 +0700

    duy/config - auth

--- src/main/java/com/api/shoesshop/repositories/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.shoesshop.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.shoesshop.entities.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    OrderItem findByOrderIdAndProductVariantId(Long orderId, Long productVaraintId);
-}
--- src/main/java/com/api/shoesshop/repositories/VariantValueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.shoesshop.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.shoesshop.entities.VariantValue;
-
-public interface VariantValueRepository extends JpaRepository<VariantValue, Long> {
-
-}
